603f0333ea9d963d1f5ef49e98bcfeaa6f29fe39
Pascal Véron <veron@univ-tln.fr>
2021-06-10 17:17:22 +0200

    Rename patches folder

---
+++ HQC/patches_AVX2_AVX512/AVX2/hqc-192/hqc.h
@@ -0,0 +1,16 @@
+#ifndef HQC_H
+#define HQC_H
+
+/**
+ * @file hqc.h
+ * @brief Functions of the HQC_PKE IND_CPA scheme
+ */
+
+#include <stdint.h>
+#include <immintrin.h>
+
+void hqc_pke_keygen(unsigned char* pk, unsigned char* sk);
+void hqc_pke_encrypt(uint64_t *u, uint64_t *v, uint64_t *m, unsigned char *theta, const unsigned char *pk);
+void hqc_pke_decrypt(uint64_t *m, const __m256i *u, const uint64_t *v, const unsigned char *sk);
+
+#endif
